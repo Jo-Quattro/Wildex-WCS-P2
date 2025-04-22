734c4a41df9a2d98626d811145b18fbdde4dd75a
Hans Siemon <hanssiemon@Hanss-MacBook-Air.local>
2025-04-22 15:56:18 +0200

    Nous avons défini et ajouter des props dans une fonction

--- client/src/components/Pokemons.tsx
+++ client/src/components/Pokemons.tsx
@@ -1,10 +1,56 @@
 import PokemonCard from "./PokemonCard";
 import "./PokemonCard.css";
 
+const pokemonGen1 = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    type: "Grass",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    id: 2,
+    name: "Ivysaur",
+    type: "Grass",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+  },
+
+  {
+    id: 3,
+    name: "Venusaur",
+    type: "Grass",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+  },
+
+  {
+    id: 4,
+    name: "Charmander",
+    type: "Fire",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+
+  {
+    id: 5,
+    name: "Charmeleon",
+    type: "Fire",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+  },
+];
+
 function Pokemons() {
   return (
     <section className="pokemon-list">
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonGen1[0]} />
+      <PokemonCard pokemon={pokemonGen1[1]} />
+      <PokemonCard pokemon={pokemonGen1[2]} />
+      <PokemonCard pokemon={pokemonGen1[3]} />
+      <PokemonCard pokemon={pokemonGen1[4]} />
     </section>
   );
 }
