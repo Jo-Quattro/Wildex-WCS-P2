0e236bddd06775b466eab94e0459edddb0ddd5ea
John Doe <david.arfi69100@hotmail.fr>
2025-04-23 11:47:05 +0200

    creation de l'appel API

--- client/src/pages/Pokemons.tsx
+++ client/src/pages/Pokemons.tsx
@@ -1,57 +1,21 @@
-import PokemonCard from "../components/PokemonCard";
+import { useEffect, useState } from "react";
 
-const pokemonGen1 = [
-  {
-    id: 1,
-    name: "Bulbasaur",
-    type: "Grass",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-
-  {
-    id: 2,
-    name: "Ivysaur",
-    type: "Grass",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-  },
-
-  {
-    id: 3,
-    name: "Venusaur",
-    type: "Grass",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
-  },
-
-  {
-    id: 4,
-    name: "Charmander",
-    type: "Fire",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-  },
-
-  {
-    id: 5,
-    name: "Charmeleon",
-    type: "Fire",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
-  },
-];
+interface pokemonType {
+  name: string;
+}
 
 function Pokemons() {
-  return (
-    <section className="pokemon-list">
-      <PokemonCard pokemon={pokemonGen1[0]} />
-      <PokemonCard pokemon={pokemonGen1[1]} />
-      <PokemonCard pokemon={pokemonGen1[2]} />
-      <PokemonCard pokemon={pokemonGen1[3]} />
-      <PokemonCard pokemon={pokemonGen1[4]} />
-    </section>
-  );
+  const [pokemons, setPokemons] = useState<pokemonType[] | null>(null);
+  useEffect(() => {
+    // if (pokemons.length === 0) {
+    fetch("https://pokeapi.co/api/v2/generation/1/")
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemons(data.pokemon_species);
+      });
+    // }
+  }, []);
+  return <h1>{pokemons?.length}</h1>;
 }
 
 export default Pokemons;
