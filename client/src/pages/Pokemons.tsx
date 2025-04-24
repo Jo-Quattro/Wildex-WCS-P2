7b5bad2f4aab85addc13d5c03ce9720f64b9e505
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-04-24 17:26:10 +0200

    added route to Pokemons pages when selected from home

--- client/src/pages/Pokemons.tsx
+++ client/src/pages/Pokemons.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 // This props define the types of the data we get from the API (an array of objects)
 interface genResponse {
   pokemon_species: pokemonType[];
 }
-
 // Defining the types for the Pokemon data we put in our state
 interface pokemonTypeType {
   type: {
@@ -13,6 +13,8 @@ interface pokemonTypeType {
 }
 interface pokemonType {
   name: string;
+  url: string;
+
   id: number;
   types: pokemonTypeType[];
   sprites: {
@@ -23,21 +25,27 @@ interface pokemonType {
     };
   };
 }
-//
+
+function getId(url: string) {
+  const id = url.split("/").slice(-2)[0];
+  return id;
+}
 
 function Pokemons() {
+  const { id } = useParams();
+
   const [pokemons, setPokemons] = useState<pokemonType[]>([]);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/generation/1/") //Fetching the 151 first Pokemons
+    fetch(`https://pokeapi.co/api/v2/generation/${id}`) //Fetching the 151 first Pokemons
       .then((response) => response.json())
       .then((data: genResponse) => {
         const pokeSpecie = data.pokemon_species;
         // We map through the array of objects and fetch each Pokemon individually
         const allPokemons = pokeSpecie.map((pokeSpecie) =>
-          fetch(`https://pokeapi.co/api/v2/pokemon/${pokeSpecie.name}`).then(
-            (response) => response.json(),
-          ),
+          fetch(
+            `https://pokeapi.co/api/v2/pokemon/${getId(pokeSpecie.url)}`,
+          ).then((response) => response.json()),
         );
         // We use Promise.all to wait for all the fetches to finish before resolving the promise
         Promise.all(allPokemons).then((results: pokemonType[]) => {
@@ -45,7 +53,7 @@ function Pokemons() {
           console.info(results);
         });
       });
-  }, []);
+  }, [id]);
 
   return (
     <>
