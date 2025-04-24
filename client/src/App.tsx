b78dd5879406b74fcae655e970ec85ac35ebc53e
Nicolas GIRARD <NG38@outlook.fr>
2025-04-24 15:53:13 +0200

    création du dossier font dans src et ajout de la police. test ok. suppression des liens  google font dans l'html

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./font/MonaSans-Regular.ttf";
 import { Outlet } from "react-router";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
