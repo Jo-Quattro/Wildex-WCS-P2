14feb5596f43178f6d4e41b8bb94c96a33ad9c2c
vianelbn <vb.nyamsi@yahoo.fr>
2025-05-15 17:02:19 +0200

    uptate css burgermenu

--- client/src/components/BurgerMenu.tsx
+++ client/src/components/BurgerMenu.tsx
@@ -37,15 +37,33 @@ const BurgerMenu = () => {
       <div className={menu_class} />
 
       <div className={generation_class}>
-        <Link to="/Pokemons/1">gen1</Link>
-        <Link to="/Pokemons/2">gen2</Link>
-        <Link to="/Pokemons/3">gen3</Link>
-        <Link to="/Pokemons/4">gen4</Link>
-        <Link to="/Pokemons/5">gen5</Link>
-        <Link to="/Pokemons/6">gen6</Link>
-        <Link to="/Pokemons/7">gen7</Link>
-        <Link to="/Pokemons/8">gen8</Link>
-        <Link to="/Pokemons/9">gen9</Link>
+        <Link className="genone" to="/Pokemons/1">
+          Gen 1 <p>sortie:1996</p>
+        </Link>
+        <Link className="gentwo" to="/Pokemons/2">
+          Gen 2 <p>sortie:1999</p>
+        </Link>
+        <Link className="genthree" to="/Pokemons/3">
+          Gen 3 <p>sortie:2002</p>
+        </Link>
+        <Link className="genfour" to="/Pokemons/4">
+          Gen 4 <p>sortie:2006</p>
+        </Link>
+        <Link className="genfive" to="/Pokemons/5">
+          Gen 5 <p>sortie:2011</p>
+        </Link>
+        <Link className="gensix" to="/Pokemons/6">
+          Gen 6 <p>sorie:2013</p>
+        </Link>
+        <Link className="genseven" to="/Pokemons/7">
+          Gen 7 <p>sortie:2016</p>
+        </Link>
+        <Link className="geneight" to="/Pokemons/8">
+          Gen 8 <p>sortie2019</p>
+        </Link>
+        <Link className="gennine" to="/Pokemons/9">
+          Gen 9 <p>sortie:2022</p>
+        </Link>
       </div>
     </aside>
   );
